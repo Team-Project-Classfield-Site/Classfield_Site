cda3f24e7506aff286437838725ea82545693b16
Jar1kkk <aroslavtroancuk@gmail.com>
2026-02-19 22:22:00 +0200

    small_seciryti_update

--- Backend/classfield/settings.py
+++ Backend/classfield/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
-SECRET_KEY = 'django-insecure-tymchasovyi-kliuch-dlia-rozrobky-12345'
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.getenv('SECRET_KEY_DJANGO')
+SECRET_KEY = os.getenv('SECRET_KEY_DJANGO')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
