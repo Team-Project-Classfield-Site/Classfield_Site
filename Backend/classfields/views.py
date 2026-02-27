deae87d7ab0eafd7cff90eb3fe34514d5c4ccfdb
Yankee <167356068+uankee@users.noreply.github.com>
2026-02-27 17:10:46 +0200

    Add IsOwnerOrReadOnly permission and update ClassfieldViewSet to enforce ownership

--- Backend/classfields/views.py
+++ Backend/classfields/views.py
@@ -2,21 +2,17 @@ from rest_framework import viewsets, permissions
 from .models import Classfield
 from .serializers import ClassfieldSerializer
 from .pagination import ClassfieldPagination
+from .permissions import IsOwnerOrReadOnly 
 from users.models import UserClassfield
 
 class ClassfieldViewSet(viewsets.ModelViewSet):
     queryset = Classfield.objects.all().order_by('-date')
     serializer_class = ClassfieldSerializer
     pagination_class = ClassfieldPagination
-    # permissions_classes = [permissions.IsAuthenticatedOrReadOnly] # Розкоментувати, коли буде готова авторизація
+    
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
-        # Автоматично прив'язуємо оголошення до поточного користувача
-        # заглушка/ беремо першого юзера, якщо авторизація ще не налаштована
-        # В майбутньому замінити на: owner = UserClassfield.objects.get(user=self.request.user)
-        try:
-             #поки немає авторизації
-            owner = UserClassfield.objects.first() 
-            serializer.save(owner=owner)
-        except:
-            serializer.save()
+        user_profile = UserClassfield.objects.get(user=self.request.user)
+        
+        serializer.save(owner=user_profile)
